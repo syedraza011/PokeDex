30eb51346dd3389a8c6102e56b2d2ca3c96a9f10
Syed Raza <syed.raza011@gmail.com>
2023-08-06 15:08:11 -0400

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Syed Raza on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
