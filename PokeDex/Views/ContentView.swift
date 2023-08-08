a2806904497d3cc3fb3ccbd40755ee026569722a
Syed Raza <syed.raza011@gmail.com>
2023-08-08 11:45:02 -0400

    final fixtures

--- PokeDex/Views/ContentView.swift
+++ PokeDex/Views/ContentView.swift
@@ -5,50 +5,43 @@
 //  Created by Syed Raza on 8/6/23.
 
 import SwiftUI
-
 struct ContentView: View {
     @ObservedObject var viewModel = PokeDexViewModel()
     
     var body: some View {
         NavigationView {
             VStack {
-                ScrollView (showsIndicators: false){
-                   
-                    ForEach(viewModel.allPokeDex, id: \.id) { singlePoke in
+                ScrollView (showsIndicators: false) {
+                    ForEach(Array(viewModel.allPokeDex.enumerated()), id: \.1.id) { index, singlePoke in
                         VStack {
-                            HStack{
-                                
+                            HStack {
                                 PokeImage(singlePoke.name)
-                                    .bold()
-                                VStack(alignment: .leading){
                                    
+                                VStack(alignment: .leading) {
                                     Text(singlePoke.name.capitalized)
-                                    Text("#")
-                            
-                                    HStack{
+                                    Text("#\(index + 1)")
+                                        .foregroundColor(.gray)
+                                    HStack {
                                         Text("see")
-                                        NavigationLink ("More"){
-                                            Text("Hello")
+                                        NavigationLink ("More") {
+                                            Text(singlePoke.name.capitalized)
+                                            
                                         }
                                     }
-                                    
                                 }.padding(20)
                             }
                         }.padding(30)
-                  
                     }
                 }
                 .onAppear() {
                     viewModel.getPokeDex()
                 }
-                
             }
             .padding()
             .navigationBarTitle("PokéDex")
-            
         }
     }
-    
+
     @ViewBuilder // Specify that this function returns a View
     private func PokeImage(_ pokeName: String) -> some View {
         if let imageURL = URL(string: "https://img.pokemondb.net/artwork/large/\(pokeName.lowercased()).jpg") {
